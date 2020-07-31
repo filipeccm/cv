2423df3ed5d5b7e68fe219fe85e195b4d9b635bd
Filipe <filipeccm@gmail.com>
2020-07-31 11:06:10 +0200

    Updates

---
+++ precache-manifest.81ec2ddeb8f992aa101e5b08fd1a267f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43baaa5222b82519fc935de85ec1441",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "ff86e697706983ba2ad8",
+    "url": "/cv/static/css/2.404ebb63.chunk.css"
+  },
+  {
+    "revision": "128b6949b25225da6d37",
+    "url": "/cv/static/css/3.d70736d0.chunk.css"
+  },
+  {
+    "revision": "964943ce1868a2fbdeb2",
+    "url": "/cv/static/css/main.c081ca5d.chunk.css"
+  },
+  {
+    "revision": "ff86e697706983ba2ad8",
+    "url": "/cv/static/js/2.a37fb0da.chunk.js"
+  },
+  {
+    "revision": "128b6949b25225da6d37",
+    "url": "/cv/static/js/3.7a0dab8a.chunk.js"
+  },
+  {
+    "revision": "d1d57a75c91d9e3ead08",
+    "url": "/cv/static/js/4.c4a1f2e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/4.c4a1f2e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964943ce1868a2fbdeb2",
+    "url": "/cv/static/js/main.ca213b69.chunk.js"
+  },
+  {
+    "revision": "c606811cd594722325b8",
+    "url": "/cv/static/js/runtime-main.050c6428.js"
+  },
+  {
+    "revision": "d83ccc1ad22f3d0b671a8ad2572ba869",
+    "url": "/cv/static/media/MyLibraryCover.d83ccc1a.svg"
+  },
+  {
+    "revision": "0f3b12d8d06adcf2d9b75d1174e8c1af",
+    "url": "/cv/static/media/afslogo.0f3b12d8.svg"
+  },
+  {
+    "revision": "3fc3f90a801c99ea7cb1e51488ed1859",
+    "url": "/cv/static/media/coolhowLogo.3fc3f90a.svg"
+  },
+  {
+    "revision": "857e75fe09dae67787cec8087dfbe248",
+    "url": "/cv/static/media/etch-a-sketch-cover.857e75fe.svg"
+  },
+  {
+    "revision": "38e0a9fcfdfecea3b499449f7e494b68",
+    "url": "/cv/static/media/github-logo.38e0a9fc.svg"
+  },
+  {
+    "revision": "29a02864072994c940f5454105ae4ba7",
+    "url": "/cv/static/media/linkedin-logo.29a02864.svg"
+  },
+  {
+    "revision": "52732962fcbeab92f8c0fdb1af084dad",
+    "url": "/cv/static/media/logo-once.52732962.gif"
+  },
+  {
+    "revision": "3680ea61d2d71632da14be15a9ff6bd2",
+    "url": "/cv/static/media/mplogo.3680ea61.svg"
+  },
+  {
+    "revision": "afd19fe5a6413ebdcb9b33f9e422547e",
+    "url": "/cv/static/media/todo-list.afd19fe5.svg"
+  },
+  {
+    "revision": "4864b1e4f9b781854b638cdc686141fa",
+    "url": "/cv/static/media/weatherAPI-cover.4864b1e4.svg"
+  }
+]);
